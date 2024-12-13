4397286fce7b51c92b8f222a642710d962e6e082
Ayushi Karn <53132904+ayushi186@users.noreply.github.com>
2024-12-13 16:56:02 +1100

    Update dbConfig.ts

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -4,11 +4,11 @@ export async function connect() {
     try{ mongoose.connect(process.env.MONGO_URI!)
         const connection = mongoose.connection;
         connection.on('connected' , () =>{
-            debugger
-            console.log('MOngoDB connected successfully')
+        
+            
         })
         connection.on('error' , () =>{
-            console.log('MOngoDB connectio erorr')
+           
         })
     }
     catch(error){
